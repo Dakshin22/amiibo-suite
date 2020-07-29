a7b62ebbd7328e26ee91e13522a12c30b046a131
Dakshin R <drathan45@gmail.com>
2020-07-29 12:02:42 -0400

    sorting with insertion sort

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import Card from "./Card";
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
 const App = () => {
   const [query, setQuery] = useState("");
   const [results, setResults] = useState([]);
@@ -21,13 +24,13 @@ const App = () => {
     );
     const data = await response.json();
     setResults(data.amiibo);
-    console.log(data.amiibo);
+
   };
 
   const handleChange = (e) => {
     const { value } = e.target;
     setQuery(value);
-    console.log(query);
+
   };
   const handleSearch = (e) => {
     e.preventDefault();
@@ -38,8 +41,53 @@ const App = () => {
   const handleTypeChange = (e) => {
     const { value } = e.target;
     setType(value);
-    console.log(`Type: ${type}`);
+
   };
+
+  const reorder = (resultInput) => {
+
+
+    let resultOrig = resultInput
+    let temp = {}
+    let j = 0 
+  for(let i = 0; i<resultOrig.length; i++)
+  {
+    j = i
+    while(j>0 && newerThan(resultOrig[j].release.na, resultOrig[j-1].release.na))
+    {
+      
+      temp = resultOrig[j]
+      resultOrig[j] = resultOrig[j-1]
+      resultOrig[j-1] = temp
+      j = j-1;
+    }
+
+  
+  }
+  for(let i = 0; i<resultOrig.length; i++)
+  {
+    console.log(resultOrig[i].release.na)
+  }
+  return resultOrig;
+}
+
+ const newerThan = (left, right) =>
+ {
+   if(left && right)
+   {
+  const newLeft = parseInt(left.replace(/-/g, ''))
+  const newRight = parseInt(right.replace(/-/g, ''))
+
+  console.log(newLeft)
+  console.log(newRight)
+  const ret = newLeft > newRight ? true : false
+  return ret
+   }
+   else
+   {
+     return 0
+   }
+  }
 
   return (
     <div className="App">
@@ -60,9 +108,14 @@ const App = () => {
         <button>Click to Search</button>
       </form>
 
+ 
+ <CircularProgressbar value={54} text={`${54}%`} />
       {results &&
-        results.map((result) => (
+        console.log(reorder(results))}
+
+       { results.map((result) => (
           <Card
+            key = {result.image}
             name={result.name}
             series={result.gameSeries}
             image={result.image}
