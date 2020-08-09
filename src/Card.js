62f2c91199ab012fe82ee7c7587d6d09f5da1f98
Dakshin R <drathan45@gmail.com>
2020-08-09 15:23:57 -0400

    implement callback in card

--- src/Card.js
+++ src/Card.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import style from './Card.module.css'
 
 const Card = (props) => {
+    const [collected, setCollected] = useState(false)
+    const handleCheck = (e) =>
+    {
+        setCollected(e.target.checked)
+        props.collectCallback(props.object, e.target.checked)
+        console.log(`this is from card.js ${props.object}`)
+    }
     return(
         <div className = {style.Card}>
+            <input type="checkbox" value={collected} onChange = {handleCheck}/>
             <h1>{props.name}</h1>
             <img className = {style.image} src = {props.image} alt = "amiibo"/>
             <h3>from {props.series}</h3>
