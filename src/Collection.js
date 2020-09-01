8e44223fd356c0c0cd25656bc595eb22e3eab696
Dakshin R <drathan45@gmail.com>
2020-09-01 11:46:49 -0400

    Add default text

--- src/Collection.js
+++ src/Collection.js
@@ -8,11 +8,14 @@ const Collection = (props) =>
         <h1 className = "Header">
         Collection:
         </h1>
+        {props.collectionArr.length > 0 ? 
         <ul className = "CollectionList">
           {props.collectionArr.map((item) => {
             return <li key={item.image}>{item.name} - {item.amiiboSeries}</li>;
           })}
         </ul>
+        :
+        <p>There's nothing in your collection yet, click checkboxes on the reusults that you want to add to your collection.</p>}
         </div>
     )
 }
