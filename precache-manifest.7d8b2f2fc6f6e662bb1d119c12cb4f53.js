d962fcb56e6e3fdfafad762dacf2e5fa01316589
Dakshin R <drathan45@gmail.com>
2020-10-04 12:15:47 -0400

    Updates

---
+++ precache-manifest.7d8b2f2fc6f6e662bb1d119c12cb4f53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17a0cb38d2c262f2a149e90211e5719b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1f841edcb375390e0b71",
+    "url": "./static/css/2.f524894f.chunk.css"
+  },
+  {
+    "revision": "139b161ce59c02a48ea1",
+    "url": "./static/css/main.73d99716.chunk.css"
+  },
+  {
+    "revision": "1f841edcb375390e0b71",
+    "url": "./static/js/2.c2a60ae6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.c2a60ae6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "139b161ce59c02a48ea1",
+    "url": "./static/js/main.98ca5d85.chunk.js"
+  },
+  {
+    "revision": "ff9c839120ac837fc4c0",
+    "url": "./static/js/runtime-main.0e4214a7.js"
+  }
+]);
