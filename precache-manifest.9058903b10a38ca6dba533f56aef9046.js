d855bdecb85b58671c0c4af68d663d814e9fc51c
Dakshin R <drathan45@gmail.com>
2020-10-04 12:03:54 -0400

    Updates

---
+++ precache-manifest.9058903b10a38ca6dba533f56aef9046.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8196ec46dbd9ca8e91e733285f65dcb",
+    "url": "/amiibo-suite/index.html"
+  },
+  {
+    "revision": "1f841edcb375390e0b71",
+    "url": "/amiibo-suite/static/css/2.f524894f.chunk.css"
+  },
+  {
+    "revision": "daee024b7b69a45425ec",
+    "url": "/amiibo-suite/static/css/main.73d99716.chunk.css"
+  },
+  {
+    "revision": "1f841edcb375390e0b71",
+    "url": "/amiibo-suite/static/js/2.c2a60ae6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/amiibo-suite/static/js/2.c2a60ae6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daee024b7b69a45425ec",
+    "url": "/amiibo-suite/static/js/main.7dbc6b7b.chunk.js"
+  },
+  {
+    "revision": "bc40e5bfef5607c09ddc",
+    "url": "/amiibo-suite/static/js/runtime-main.1a5f4ef6.js"
+  }
+]);
